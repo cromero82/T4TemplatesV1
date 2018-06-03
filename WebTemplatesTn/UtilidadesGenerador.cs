6655f2ce5ae8513d3c2ca82fffc5b1e69b8475b7
carlos.romero <carlos.romero@ito-software.com>
2018-06-03 11:39:58 -0500

    inicial

---
+++ WebTemplatesTn/UtilidadesGenerador.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebTemplatesTn
+{
+    public static class UtilidadesGenerador
+    {
+        public static string EntidadFormatoPresentacion(string entidad)
+        {
+            var entidadPresentacion = entidad.Substring(0, 1);
+            for (int i = 0; i < entidad.Length; i++)
+            {
+                //if(entidad.Substring(i,1).i
+                if (i > 0)
+                {
+                    if (char.IsLower(entidad[i]))
+                    {
+                        entidadPresentacion = string.Concat(entidadPresentacion, entidad[i]);
+                    }
+                    else
+                    {
+                        entidadPresentacion = string.Concat(entidadPresentacion, " ", entidad[i]);
+                        //entidadPresentacion.Concat(""; entidad[i]);
+                    }
+                }
+            }
+            return entidadPresentacion;
+        }
+    }
+}
